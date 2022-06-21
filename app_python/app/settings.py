92c8526d4a163e44219591b4270e0c2999c477a3
lidaev <lidiya.edilgiriyeva@gmail.com>
2022-06-21 11:46:45 +0600

    Add test log file

--- app_python/app/settings.py
+++ app_python/app/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(os.path.join(BASE_DIR.parent, '.env'))
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-LOG_FILENAME = os.environ.get('LOG_FILENAME', '')
+LOG_FILENAME = os.environ.get('LOG_FILENAME', 'app.log')
 
 DEBUG = False
 
